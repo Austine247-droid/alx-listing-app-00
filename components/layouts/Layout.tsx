9685917e945712f5c271b73963b35d55f142e38e
Austine247-droid <ugberaeseoa@gmail.com>
2025-06-01 21:49:22 +0000

    updated files

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,27 +1,12 @@
-// import React, { ReactNode } from "react";
-// import Header from "./Header";
-// import Footer from "./Footer";
-
-// interface LayoutProps {
-//   children: ReactNode;
-// }
-
-// const Layout: React.FC<LayoutProps> = ({ children }) => {
-//   return (
-//     <>
-//       <Header />
-//       <main className="min-h-screen">{children}</main>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default Layout;
-
+import React, { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const Layout: React.FC = ({ children }) => {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Header />
